0ee3853d3954afe028556da04c5692ca81f6e44d
JSlovak <jasmine.slovak@gmail.com>
2017-04-19 12:05:34 -1000

    handlers for static pages, wild card and 404 active on main server

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const example middleWare = require('./middleware/filename');
 
-
-//Create server
 const app = express();
 const port = 3000;
 
+// handle static requests
+app.use(express.static('public'));
+
+// handle 404 pages
+app.get(`*`, (req,res)=>{
+  res.send('404');
+});
+
+//Create server
 const server = app.listen(port, function () {
   const host = server.address().address;
   const port = server.address().port;
